33e98f767f4b974df863c59a4475089f16cc6b10
Kauan-Azevedo <kauanazevedo2010@hotmail.com>
2022-11-28 16:14:19 -0300

    feat: bebidas, usuarios added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BebidasModule } from './bebidas/bebidas.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
 
 @Module({
-  imports: [],
+  imports: [BebidasModule, UsuariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
